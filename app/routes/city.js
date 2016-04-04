383f4ef6b4ab9ee1075c7e456d33fad3053d5f3d
Jared Beckler <jared.beckler@gmail.com>
2016-04-03 22:45:06 -0700

    do stuff and things: WIP on last homework lesson for weekend before Ember Extended week

--- app/routes/city.js
+++ app/routes/city.js
@@ -4,11 +4,12 @@ export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('city', params.city_id);
   },
-    actions: {
-
+  
+  actions: {
     save3(params){
       var newRental = this.store.createRecord('rental', params);
       var city = params.city;
+
       city.get('rentals').addObject(newRental);
       newRental.save().then(function(){
         return city.save();
